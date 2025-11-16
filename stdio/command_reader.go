14acdd47b1c28e65565fdecf97ba4606575cbc7b
SSripilaipong <SHSnail@mail.com>
2025-11-16 19:23:59 +0700

    Implement stdio command reader

--- stdio/command_reader.go
+++ stdio/command_reader.go
@@ -1,21 +1,34 @@
 package stdio
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
 	"github.com/SSripilaipong/clipboard-helper/core"
 	"github.com/SSripilaipong/go-common/rslt"
 )
 
 // CommandReader obtains text commands from stdin.
-type CommandReader struct{}
+type CommandReader struct {
+	reader *bufio.Reader
+}
 
 // NewCommandReader wires the stdin-backed implementation.
 func NewCommandReader() *CommandReader {
-	return &CommandReader{}
+	return &CommandReader{
+		reader: bufio.NewReader(os.Stdin),
+	}
 }
 
 // ReadCommand fetches the next command from the user.
 func (s *CommandReader) ReadCommand() rslt.Of[core.Command] {
-	return rslt.Error[core.Command](errors.New("stdin command reader not implemented"))
+	fmt.Print(">>> ")
+	line, err := s.reader.ReadString('\n')
+	if err != nil {
+		return rslt.Error[core.Command](err)
+	}
+
+	return rslt.Value(core.Command(strings.TrimSpace(line)))
 }
